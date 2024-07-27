6f6e9bd956f1bcfa28647013d9b2129d7ff9e53c
unknown <you@example.com>
2024-07-27 14:10:22 +0200

    c2

---
+++ index.js
@@ -0,0 +1,13 @@
+function user()
+{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => {
+        let users = document.getElementById('users');
+        json.forEach(element => {
+            let user = document.createElement('option');
+            user.innerHTML = element.name;
+            users.appendChild(user);
+        });
+    })
+}
